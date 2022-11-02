ed079cc9452fdb5109989f9226ad9a1c59711f4a
igopalakrishnan <kingkrishna809@gmail.com>
2022-11-02 11:28:54 +0530

    create project media

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,48 +3,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
 const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [index, setIndex] = useState(1);
-    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-    const period = 2000;
-
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta);
-
-        return () => { clearInterval(ticker) };
-    }, [text])
-
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2);
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setIndex(1);
-            setDelta(500);
-        } else {
-            setIndex(prevIndex => prevIndex + 1);
-        }
-    }
 
     return (
         <section className="banner" id="home">
@@ -55,6 +16,16 @@ const Banner = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <span className="tagline">Welcome to my Projects</span>
+                                    <h4>Tools/Technologies:</h4>
+                                    <p>HTML5/CSS3, Bootstrap, React JS, Node JS, Express, MongoDB, React Native, Sketch, Google Cloud Platform, Firebase, Context API, Sanity.io.....,,</p>
+                                    <button>
+                                        <a style={{
+                                            textDecoration: "none",
+                                            color: "#fff"
+                                        }} href={"https://projects-media.netlify.app/"}  target="_blank">Media
+                                            <ArrowRightCircle size={27} />
+                                        </a>
+                                    </button>
                                 </div>}
                         </TrackVisibility>
                     </Col>
